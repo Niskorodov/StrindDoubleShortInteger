41f3c785a3bc3c5bc300dc235b6a0b530444c811
Dmitrii <103382610+Niskorodov@users.noreply.github.com>
2022-11-08 10:20:03 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String s = "";
+        String s;
         while (true) {
             s = reader.readLine();
             if (s.equals("exit"))
@@ -26,18 +26,18 @@ public class Main {
     }
 
     public static void print(Double value) {
-        System.out.println("Это тип Double, значение " + value);
+        System.out.println("Это тип Double, значение" + value);
     }
 
     public static void print(String value) {
-        System.out.println("Это тип String, значение " + value);
+        System.out.println("Это тип String, значение" + value);
     }
 
     public static void print(short value) {
-        System.out.println("Это тип short, значение " + value);
+        System.out.println("Это тип short, значение" + value);
     }
 
     public static void print(Integer value) {
-        System.out.println("Это тип Integer, значение " + value);
+        System.out.println("Это тип Integer, значение" + value);
     }
 }
